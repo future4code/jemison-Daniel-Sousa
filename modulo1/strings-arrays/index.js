412e31812943c401c034b27c3a64f1757d2b6e55
Daniel Da Silva De Sousa <daniellsousa0614@gmail.com>
2022-04-19 19:23:14 -0300

    Finalizando o Desafio

--- modulo1/strings-arrays/index.js
+++ modulo1/strings-arrays/index.js
@@ -38,14 +38,14 @@
  // Exercícios de escrita de código
 
  //Questão 01
- /*let nome = prompt("Digite seu nome:")
+ let nome = prompt("Digite seu nome:")
  let email = prompt("Digite seu email:")
 
  console.log(`seu nome é ${nome} e seu email é: ${email}`)*/
 
  //Questão 02
 
-    /* const minhasComidas = ["Pizza", "Feijoada", "Baião de dois", "Macarronada", "Salada de frutas"]
+    const minhasComidas = ["Pizza", "Feijoada", "Baião de dois", "Macarronada", "Salada de frutas"]
     console.log(minhasComidas)
     console.log(`Essas são minhas comidas preferidas:
 ${minhasComidas[0]}
@@ -56,10 +56,10 @@ ${minhasComidas[4]}`)
 
 let comidaUsuario = prompt("Digite Sua comida preferida")
 minhasComidas[1] = comidaUsuario
-console.log(minhasComidas) */
+console.log(minhasComidas)
 
 //Questão 3
- /*let listaDeTarefas = [] //podedia usar o null tambem 
+ let listaDeTarefas = [] //podedia usar o null tambem 
 
  let primeiraTarefa = prompt("Digite a sua  primeira tarefa  de hoje:")
  let segundaTarefa = prompt("Digite a  sua segunda tarefa de hoje:")
@@ -74,7 +74,21 @@ console.log(minhasComidas) */
   2.${listaDeTarefas[2]}`))
 
  listaDeTarefas.splice(tarefaRelizada,1) // ira remover oque o usuario digitar
- console.log(listaDeTarefas) */
+ console.log(listaDeTarefas) 
  
+// Desafio
+//Questão 1
+
+let qualquerFrase = "Seu esforço será recompensado!"
+let arrayFrases = [qualquerFrase.split(" ")] // No metodo split ("") retorna cada letra da frase - Usando apenas (" ") retorna apenas as palavras da frase!
+console.log(arrayFrases)
+
+//Questão 2
+
+let nomesFrutas = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"]
+console.log(`O indice da acabaxi é: ${nomesFrutas.indexOf("Abacaxi")+ 1} .`) // Já que os array o indece começa com o indeci 0, é necessario somar com 1 para ter o resultado esperado
+console.log(`O tamanho da lista de Frutas é: ${nomesFrutas.length}`)
+
+
 
 
