c8594c1daaf4648a8d63e2d163c0317651780187
Daniel Da Silva De Sousa <daniellsousa0614@gmail.com>
2022-04-27 22:24:34 -0300

    Terminado interpretacao de código

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,26 @@
+// --------Exercícios de interpretação de código -------------------
+
+//Questão 01
+//A: Console verifica se o numerá o par, ou seja, se o numero digita pelo usuario é tem resto 0.
+
+//B: Para todos os numeros pares diferente de 0.
+
+//C: Para todos os numeros impares diferente de 0.
+
+//Questão 02
+//A: O código verifica se atrás do switch se a resposta do usuario corresponde a alguns dos cases.
+
+//B: Console iré imprimir: O preço da fruta  Maçã é R$ 2.25
+
+//C: Console iria passar pelo condigo mais não iria imprir o resultado. Passando para o a resposta atribuída ao defaut. Ou seja, O preço da fruta  Pêra  é  R$  5.
+
+//Questão 03
+//A: A primeira linha está guardando a resposta do úsuario atribuída no prompt
+
+//B: Console imprime undefined. Pois, não há uma condição para um número menor que 0.
+
+//C: Sim! Pois, a variavel chamada não está no escopo global do codigo, mas no bloco do if.
+
+// ----------------Exercícios de escrita de código----------------
+
+
