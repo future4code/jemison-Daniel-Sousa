79cf58d8a25f053432c68e5d1d851035fba724e6
Daniel Da Silva De Sousa <daniellsousa0614@gmail.com>
2022-05-03 18:56:13 -0300

    Finalizado Interpretação de código

--- modulo1/lacos/index.js
+++ modulo1/lacos/index.js
@@ -1,27 +1,67 @@
+// ---- Interpletação de código -----
+
+//Questão 01 - O que o código abaixo está fazendo? Qual o resultado impresso no console?
+            /* let valor = 0
+            for(let i = 0; i < 5; i++) {
+            valor += i
+            }
+            console.log(valor) */
+
+//R: Console imprime 10.
+
+// Questão 02 - Leia o código abaixo:
+            /*
+            const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+            for (let numero of lista) {
+            if (numero > 18) {
+                    console.log(numero)
+                }
+            }*/
+
+//RA: Console imprime todos os numeros maiores que 18. Ou seja, 19, 21, 23, 25, 27, 30.
+//Rb: 
+
+//Questão 03 - Qual seria o resultado impresso no console, se o usuário digitasse o número 4 ?
+            /* const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+            let quantidadeAtual = 0
+            while(quantidadeAtual < quantidadeTotal){
+            let linha = ""
+            for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+                linha += "*"
+            }
+            console.log(linha)
+            quantidadeAtual++
+            }*/
+ //R03: Console imprime 4 linhas cada uma com a quantidade de *. Ou seja, *, **, ***, ****           
+
+
 //---- Escrita de codigo --------
 
 //Questão 01
-//pergunta ao usuario;
-//
 
-let quantosPet = Number(prompt("Quantos Pets você tem?"))
-let nomesPets = ""
-let quantidadePet = quantosPet
-
+// let quantidadePet = Number(prompt("Quantos Pets você tem?"))
 
 function saberNomePets(){
-    if(quantidadePet === 0){
-        console.log("Que pena! Você pode adotar um Pet!")
-    }else{
-        while(quantidadePet = quantosPet){
-            nomesPets = prompt("Digite o nome dos pets:")
-            const imprimeTest = [nomesPets]
-            console.log(imprimeTest)
+   if(quantidadePet === 0){
+       console.log(`Que pena! Você deve adotar um Pet!`)
+   } else {
+        let nomePet = "";
+        let nomesPet = [];
+        while(nomePet < quantidadePet){
+            let nomesTodoPets = prompt("Digite o nome de todos os seus Pets, um por um:")
+
+            nomesPet.push(nomesTodoPets)
+
+            nomePet++
         }
+        console.log(`Estes é o(s) nome (s) do(s) seu(s) Pets: ${nomesPet}! Até mais` )
     }
 }
 
+// saberNomePets()
 
+
+//Questão 02
 
 
 
