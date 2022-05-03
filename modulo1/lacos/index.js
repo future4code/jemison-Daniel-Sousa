1b72ba1cac6c889e45fab1cab004cc8f4d3d345e
Daniel Da Silva De Sousa <daniellsousa0614@gmail.com>
2022-05-03 14:51:45 -0300

    iniciando atividade

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,27 @@
+//---- Escrita de codigo --------
+
+//Questão 01
+//pergunta ao usuario;
+//
+
+let quantosPet = Number(prompt("Quantos Pets você tem?"))
+let nomesPets = ""
+let quantidadePet = quantosPet
+
+
+function saberNomePets(){
+    if(quantidadePet === 0){
+        console.log("Que pena! Você pode adotar um Pet!")
+    }else{
+        while(quantidadePet = quantosPet){
+            nomesPets = prompt("Digite o nome dos pets:")
+            const imprimeTest = [nomesPets]
+            console.log(imprimeTest)
+        }
+    }
+}
+
+
+
+
+
