e7549d2bbd6b09ebff71a06bbc9b77a4b6b8d1cc
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-07-05 21:46:53 -0300

    finalizando mapeamento do componento array

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -9,63 +9,48 @@ const MainContainer = styled.div`
   align-items: center;
 `
 
-const listaPost = [
-  {
-    nomeUsuario: "Paulinha",
-    fotoUsuario: 'https://picsum.photos/30/50',
-    fotoPost: 'https://picsum.photos/200/150'
-  },
-
-  {
-    nomeUsuario: "Daniel Sousa",
-    fotoUsuario:'https://picsum.photos/20/50',
-    fotoPost: 'https://picsum.photos/180/150'
-  },
-
-  {
-    nomeUsuario: "Andreane Cardoso",
-    fotoUsuario: 'https://picsum.photos/90/50',
-    fotoPost:'https://picsum.photos/210/150'
-
-  },
-
-  {
-    nomeUsuario:"Marlene Cavalcante",
-    fotoUsuario: 'https://picsum.photos/85/50',
-    fotoPost: 'https://picsum.photos/215/150'
-
-  }
-];
-
-console.log(listaPost)
-
 function App() {
+
+  const listaPost = [
+    {
+      nomeUsuario: "Paulinha",
+      fotoUsuario: 'https://picsum.photos/30/50',
+      fotoPost: 'https://picsum.photos/200/150'
+    },
+  
+    {
+      nomeUsuario: "Daniel Sousa",
+      fotoUsuario:'https://picsum.photos/20/50',
+      fotoPost: 'https://picsum.photos/180/150'
+    },
+  
+    {
+      nomeUsuario: "Andreane Cardoso",
+      fotoUsuario: 'https://picsum.photos/90/50',
+      fotoPost:'https://picsum.photos/210/150'
+  
+    },
+  
+    {
+      nomeUsuario:"Marlene Cavalcante",
+      fotoUsuario: 'https://picsum.photos/85/50',
+      fotoPost: 'https://picsum.photos/215/150'
+  
+    }
+  ];
+  
+  const informacoesLista = listaPost.map((usuario, index) =>{
+    return(
+      <Post key={index}
+        nomeUsuario = {usuario.nomeUsuario}
+        fotoUsuario = {usuario.fotoUsuario}
+        fotoPost = {usuario.fotoPost}
+      />
+    )
+  })
 return(
   <MainContainer>
-           <Post
-            nomeUsuario={'Paulinha'}
-            fotoUsuario={'https://picsum.photos/30/50'}
-            fotoPost={'https://picsum.photos/200/150'}
-          />
-
-          <Post
-            
-            nomeUsuario={'Daniel Sousa'}
-            fotoUsuario={'https://picsum.photos/20/50'}
-            fotoPost={'https://picsum.photos/180/150'}
-          />
-
-          <Post
-            nomeUsuario={'Andreane Cardoso'}
-            fotoUsuario={'https://picsum.photos/90/50'}
-            fotoPost={'https://picsum.photos/210/150'}
-          />
-
-          <Post
-            nomeUsuario={'Marlene Cavalcante'}
-            fotoUsuario={'https://picsum.photos/85/50'}
-            fotoPost={'https://picsum.photos/215/150'}
-          />
+        {informacoesLista} 
    </MainContainer>
 )
 
