a3bc96204f6859f3d4f2197eb548f1c6c28cfd74
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-07-07 22:27:50 -0300

    adicionando inputs

--- modulo3/projeto-whatslab/whatslab-projeto/src/components/Main/Main.js
+++ modulo3/projeto-whatslab/whatslab-projeto/src/components/Main/Main.js
@@ -4,9 +4,34 @@ import {Sidebar,ContinerPrincipal,ConteudoPrincipal, } from './StyleMain'
 
 
 export function Main (){
+    const [destinatario, setDestinatario] = useState("");
+    const [mensagem, setMensagem] = useState("")
+    const [novaMensagem, setNovaMensagem] = useState([
+        {
+            nome: "",
+            mensagem: "",
+        }
+
+    ]);
+      
+    const handleDestinatario = (e) =>{
+        setDestinatario(e.target.value)
+    }
     
-        
-    
+    const handleMensagem = (e) =>{
+        setMensagem(e.target.value)
+    }
+
+
+    const newMensagem =() =>{
+        return(
+            <div key={index}>
+                <p></p>
+            </div>
+
+        )
+    }
+
     return (
         <ContinerPrincipal>
             <Sidebar> Sidebar</Sidebar>
@@ -16,11 +41,14 @@ export function Main (){
                     <label>Destinatatio</label>
                     <input
                         placeholder='Destinatario..'
+                        value={destinatario}
+                        onChange={handleDestinatario}
                     />
 
                     <input
                         placeholder='Digite sua mensagem...'
-                    
+                        value={mensagem}
+                        onChange={handleMensagem}
                     />
 
                     <button>Enviar</button>
