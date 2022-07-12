bb97919f5618dfcaf914b47397168e24046a502a
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-07-12 12:28:59 -0300

    iniciando formulario-etapa1

--- modulo3/rend-condicional-form/labenuforms/src/Components/Etapa1/Etapa1.js
+++ modulo3/rend-condicional-form/labenuforms/src/Components/Etapa1/Etapa1.js
@@ -5,6 +5,22 @@ export function Etapa1(){
     return(
         <>
             <h1>Etapa 1- Dados Gerais</h1>
+            <form>
+                <ol>
+                    <li>Qual o seu nome</li>
+                    <input></input>
+
+                    <li>Qual sua idade:</li>
+                    <input></input>
+
+                    <li>Qual o seu e-mail</li>
+                    <input></input>
+
+                    <li>Qual sua escolaridade</li>
+                    <input></input>
+                </ol>
+                
+            </form>
         </>
     )
 }
