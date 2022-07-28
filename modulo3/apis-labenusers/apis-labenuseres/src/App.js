c73458a8da3682731b9aab2c93595020a7ceb1ad
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-07-28 14:58:43 -0300

    criando requisições Post

--- modulo3/apis-labenusers/apis-labenuseres/src/App.js
+++ modulo3/apis-labenusers/apis-labenuseres/src/App.js
@@ -1,33 +1,11 @@
-import { useState } from "react";
+
 import { TelaDeCastro } from "./components/TelaDeCadastro/TelaDeCadastro";
 
 
 function App() {
-  const [nomeCadastro, setNomeCadastro] = useState("")
-  const [emailCadastro, setEmailCadastro] = useState("")
-
-  const [users, setUseres] = useState([]) // 1
+  
 
 
-  // 2
-  const mapeaUseres = users.map((usuarios, index)=> {
-
-    return (
-      <div>
-          {usuarios.name}
-      </div>
-    )
-  })
-
-  // 3
-
-  const url = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users"
-
-  const serviceHeaders = {
-    headers: {
-        Authorization: 'daniel-sousa-jemison'
-    }
-}
 
 
   return (
--- modulo3/apis-labenusers/apis-labenuseres/src/components/TelaDeCadastro/TelaDeCadastro.js
+++ modulo3/apis-labenusers/apis-labenuseres/src/components/TelaDeCadastro/TelaDeCadastro.js
@@ -1,23 +1,113 @@
-
+import axios from "axios";
+import { useState, useEffect} from "react";
 
 
 export function TelaDeCastro () {
+  const [nomeCadastro, setNomeCadastro] = useState("")
+  const [emailCadastro, setEmailCadastro] = useState("")
+
+  const [users, setUseres] = useState([]) // 1
+
+
+  //NomeCadastro
+  const handleNomeCadastro = (e) =>{
+    setNomeCadastro(e.target.value)
+  }
+
+  //emailCadastro
+
+  const handleEmailCadastro = (e) =>{
+    setEmailCadastro(e.target.value)
+  }
+
+  // 2
+  const mapeaUseres = users.map((item, index) => {
+    return(
+        <ul key={index}>
+            {item.name}
+        </ul>
+    )
+  })
+
+
+  useEffect(() => {
+    getAllUsers()
+}, [])
+
+  // 3
+
+  const url = 'https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/'
+
+  const serviceHeaders = {
+        headers: {
+            Authorization: 'daniel-sousa-jemison'
+        }
+    }
+
+     //4
+    const body = {
+        "name": nomeCadastro,
+        "email": emailCadastro,
+    }
+
+    const getAllUsers = () =>{
+        axios.get(url, serviceHeaders)
+        .then((response) => { setUseres(response.data.result.list) })
+        .catch((error)=> {console.log(error.response)})
+
+    }
+
+
+    // Requisição POST CreateUser
+
+    const postCreateUser = (e) =>{
+        e.preventDefault();
+
+        axios.post(url, body, serviceHeaders)
+        .then((response) =>{
+            alert("Usuario Cadastrado com Sucesso!")
+            getAllUsers()
+        }).catch((error)=>{
+            alert("Tente novamente :(")
+        })
+
+        setNomeCadastro("");
+        setEmailCadastro("")
+
+    }
+
+
+
+
+
+
     return (
-        <form>
-            <input
-                
-                type= "text"
-                placeholder="Digite seu nome"
-            />
 
-            <input
+        <div>
+            <ul>
+                {mapeaUseres}
+            </ul>
+            <form>
                 
-                type= "text"
-                placeholder="Digite seu e-mail"
-            />
+                <input
+                    
+                    type= "text"
+                    placeholder="Digite seu nome"
+                    value={nomeCadastro}
+                    onChange ={handleNomeCadastro}
+                />
 
-            <button> Cadastrar Usuario</button>
-        </form>
+                <input
+                    
+                    type= "text"
+                    placeholder="Digite seu e-mail"
+                    value={emailCadastro}
+                    onChange ={handleEmailCadastro}
+                />
+
+                <button onClick={postCreateUser}> Cadastrar Usuario</button>
+            </form>
+        </div>
         
     )
 
