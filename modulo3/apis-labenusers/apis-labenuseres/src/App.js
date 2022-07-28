6517320ddd9ad943647ff2503fb026f09f78d949
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-07-28 12:58:31 -0300

    criando requisições e url como variaveis

--- modulo3/apis-labenusers/apis-labenuseres/src/App.js
+++ modulo3/apis-labenusers/apis-labenuseres/src/App.js
@@ -6,7 +6,28 @@ function App() {
   const [nomeCadastro, setNomeCadastro] = useState("")
   const [emailCadastro, setEmailCadastro] = useState("")
 
-  const [users, setUseres] = useState("")
+  const [users, setUseres] = useState([]) // 1
+
+
+  // 2
+  const mapeaUseres = users.map((usuarios, index)=> {
+
+    return (
+      <div>
+          {usuarios.name}
+      </div>
+    )
+  })
+
+  // 3
+
+  const url = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users"
+
+  const serviceHeaders = {
+    headers: {
+        Authorization: 'daniel-sousa-jemison'
+    }
+}
 
 
   return (
