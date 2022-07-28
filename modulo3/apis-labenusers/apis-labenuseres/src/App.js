1d3a1ccc9335c36a1edb73e86e97ebdf87b8238b
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-07-28 11:48:42 -0300

    iniciando estados para a api

--- modulo3/apis-labenusers/apis-labenuseres/src/App.js
+++ modulo3/apis-labenusers/apis-labenuseres/src/App.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
+import { TelaDeCastro } from "./components/TelaDeCadastro/TelaDeCadastro";
 
 
 function App() {
+  const [nomeCadastro, setNomeCadastro] = useState("")
+  const [emailCadastro, setEmailCadastro] = useState("")
+
+  const [users, setUseres] = useState("")
+
+
   return (
+    <div>
+      <TelaDeCastro></TelaDeCastro>
+    </div>
     
-    <>
-     <h1> Ola</h1>
-    </>
   );
 }
 
---
+++ modulo3/apis-labenusers/apis-labenuseres/src/components/TelaDeCadastro/TelaDeCadastro.js
@@ -0,0 +1,26 @@
+
+
+
+export function TelaDeCastro () {
+    return (
+        <form>
+            <input
+                
+                type= "text"
+                placeholder="Digite seu nome"
+            />
+
+            <input
+                
+                type= "text"
+                placeholder="Digite seu e-mail"
+            />
+
+            <button> Cadastrar Usuario</button>
+        </form>
+        
+    )
+
+
+
+}
