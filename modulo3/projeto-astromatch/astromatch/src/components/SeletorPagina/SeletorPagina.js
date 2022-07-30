cc3a9b21cae2d604ed412f10064b6ea3058b928d
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-07-30 11:40:56 -0300

    criando estados e funcao condicional para a troca de tela

--- modulo3/projeto-astromatch/astromatch/src/components/SeletorPagina/SeletorPagina.js
+++ modulo3/projeto-astromatch/astromatch/src/components/SeletorPagina/SeletorPagina.js
@@ -1,7 +1,26 @@
+import React, { useEffect, useState } from 'react'
 
 
 
 export function SeletorPagina () {
+    const [profileList, setProfileList] = useState([]) // lista de perfis
+    const  [changeScreen, setChangeScreen] = useState(true) // Estado para troca de tela  usando renderização condicional 
+    const [initialList, setInitialList] = useState([]) //Estado para a lista incial da api
+
+    //Funcção Troca de Tela - Renderização condicional
+    const ScreenChange = () =>{
+        if(changeScreen) {
+          return  
+        } else if(!changeScreen){
+            return
+        }else{
+
+        }
+    }
+
+
+
+
 
     return (
         <>
