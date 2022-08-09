c3a2a45b7c2417a7ac0ef8d7f3dbf93f656d2b7c
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-08-09 09:27:12 -0300

    criando a pasta de routes

---
+++ modulo4/projeto-labex/projeto-labex/src/routes/Router.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+export function Router (){
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route/>
+
+
+
+            </Routes>
+        </BrowserRouter>
+    )
+
+
+
+}
