1109392f3d3c8f61c2655ac54175fca269c45d74
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-08-13 12:08:08 -0300

    criando estrutura page login/botões e e form

--- modulo4/projeto-labex/projeto-labex/src/pages/LoginPage/LoginPage.js
+++ modulo4/projeto-labex/projeto-labex/src/pages/LoginPage/LoginPage.js
@@ -1,10 +1,38 @@
-
-
+import React from "react"
+import { useNavigate } from "react-router-dom";
+import { Header } from "../../components/Header/Header";
+import {goUut} from "../../routes/coordinator"
+import {MasterBox, ButtonsSection, FormSection} from "./Styled"
 
 export function LoginPage () {
+
+    const navigate = useNavigate() 
+
     return (
         <>
-            <p> Login Page</p>
+            <Header />
+            <MasterBox>
+                <h1> Faça login</h1>
+                <FormSection>
+                        <label> Email</label>
+                        <input
+                            placeholder={"seuemail@.com"}
+                        
+                        />
+
+                        <label> PASSAWORD  </label>
+                        <input
+                            placeholder={"********"}
+                        
+                        />
+                </FormSection>
+
+                <ButtonsSection>
+                        <button onClick={()=>goUut(navigate)}> Voltar</button>
+                        <button >  Enviar</button>
+                </ButtonsSection>
+            </MasterBox>
+            
         </>
     )
 
--- modulo4/projeto-labex/projeto-labex/src/pages/LoginPage/Styled.js
+++ modulo4/projeto-labex/projeto-labex/src/pages/LoginPage/Styled.js
@@ -1,1 +1,30 @@
 import styled from "styled-components";
+
+
+export const MasterBox = styled.div `
+    display: flex;
+    flex-direction: column;
+
+    align-items: center;
+
+    padding: 2rem;
+
+`
+
+export const FormSection = styled.section `
+    display: flex;
+    flex-direction: column;
+    gap: 0.3rem;
+
+    padding: 1rem;
+    width: 30vw;
+
+`
+
+export const ButtonsSection = styled.section `
+    display: flex;
+    gap: 0.8rem;
+
+    margin-top: 1rem;
+
+`
