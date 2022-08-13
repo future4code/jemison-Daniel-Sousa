5ed7974319517d114653b2cf8cef0fdee6a48e55
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-08-13 11:36:13 -0300

    criando estrutura page ApplicationFormPage

--- modulo4/projeto-labex/projeto-labex/src/pages/ApplicationFormPage/ApplicationFormPage.js
+++ modulo4/projeto-labex/projeto-labex/src/pages/ApplicationFormPage/ApplicationFormPage.js
@@ -1,10 +1,66 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useRequestsData } from "../../hooks/useRequestData"
+import {ApplicationForm, MasterBox, ButtonsSection} from "./Styled"
+
+import { Header } from "../../components/Header/Header";
+import {goToHome} from "../../routes/coordinator"
 
 
 export function ApplicationFormPage () {
+     const navigate = useNavigate()   
+     const [data] = useRequestsData("/trips")
+
+     const travelList = data && data.map((travel)=>{
+        return (
+            <option key={travel.id} value={travel.id}>
+                {travel.name}
+            </option>
+        )
+     })
+
 
     return (
         <>
-            <p> Application Form Page</p>
+            <Header/>
+            <MasterBox>
+                <h1> Inscreva-se para uma das viagens</h1>
+                <ApplicationForm>
+                    <form>
+                        <select >
+                            <option value=""> escolha uma Viagem</option>
+                            {travelList}
+                        </select>
+
+                        <input 
+                            placeholder={"Nome"}
+                        
+                        />
+                        <input 
+                            placeholder={"Idade"}
+                        
+                        />
+                        <input 
+                            placeholder={"Candidatura"}
+                        
+                        />
+                        <input 
+                            placeholder={"Profissão"}
+                        
+                        />
+                        <input 
+                            placeholder={"País"}
+                        
+                        />
+                        </form>
+                </ApplicationForm>
+                <ButtonsSection>
+                    <button onClick={()=>goToHome(navigate)}> Voltar</button>
+                    <button >  Inscrever-se</button>
+                </ButtonsSection>
+            </MasterBox>
+            
+
         </>
     )
 }
--- modulo4/projeto-labex/projeto-labex/src/pages/ApplicationFormPage/Styled.js
+++ modulo4/projeto-labex/projeto-labex/src/pages/ApplicationFormPage/Styled.js
@@ -1,1 +1,39 @@
 import styled from "styled-components";
+
+export const MasterBox = styled.section `
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items:center;
+    gap: 1rem;
+
+    margin-top: 1rem;
+
+
+`
+
+
+export const ApplicationForm = styled.section `
+    display: flex;
+    justify-content: center;
+
+    padding: 1rem;
+    border: 1px solid;
+   
+
+    form {
+        display: flex;
+        flex-direction: column;
+        
+    }
+
+`
+
+
+export const ButtonsSection = styled.section `
+    display: flex;
+    gap: 0.8rem;
+
+    margin-top: 1rem;
+
+`
