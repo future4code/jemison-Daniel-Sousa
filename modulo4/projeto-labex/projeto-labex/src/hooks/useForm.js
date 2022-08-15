b40bf6a0b516399b473c3c0b91c06956f1ffd3cd
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-08-15 12:24:40 -0300

    delete useRequestPost - add. Useform

---
+++ modulo4/projeto-labex/projeto-labex/src/hooks/useForm.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import {BASE_URL} from "../constants/urls"
+
+export function useForm (endpoint, body, token) {
+
+   
+
+    return[]
+}
