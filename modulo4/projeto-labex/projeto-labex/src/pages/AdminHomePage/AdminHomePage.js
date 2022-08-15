52ec6f0356f5c7f5d3d9e34be2b20c471c6f8bb4
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-08-15 12:48:54 -0300

    inciando pag adminHomePage

--- modulo4/projeto-labex/projeto-labex/src/pages/AdminHomePage/AdminHomePage.js
+++ modulo4/projeto-labex/projeto-labex/src/pages/AdminHomePage/AdminHomePage.js
@@ -1,10 +1,37 @@
-
+import React from "react"
+import { useNavigate } from "react-router-dom"
+import { useRequestsData } from "../../hooks/useRequestData"
 
 export function AdminHomePag () {
+    const navigate = useNavigate()
+    const [data, isLoading, error] = useRequestsData("/trips")
+
+    const listTrips = data && data.map((trip)=>{
+        return (     
+            <div  key={trip.id}>
+                 <div>
+                     <p> {trip.name}</p>
+                     
+               </div>
+            </div>      
+        )
+    })
 
     return(
         <>
-            <p> Admin Home Page</p>
+            <h1>Painel Administrativo</h1>
+
+            <section>
+                <button > Voltar</button>
+                <button> Criar Viagem</button>
+                <button>Logout</button>
+            </section>
+
+            <div>
+                {!isLoading  && <p> Carreango viagens</p>}
+                {!isLoading && error && (<p>Ocorreu um erro</p>)}
+                {isLoading && data && data.length > 0 && (listTrips)}
+            </div>
         </>
     )
 
