2794c566563ebf8fda5f173799e23f3f8cd1c9a8
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-08-16 09:36:59 -0300

    iniciando formulario

--- modulo4/projeto-labex/projeto-labex/src/pages/CreateTripPage/CreateTripPage.js
+++ modulo4/projeto-labex/projeto-labex/src/pages/CreateTripPage/CreateTripPage.js
@@ -1,10 +1,31 @@
+import React from "react"
+import { useNavigate } from "react-router-dom"
+import { useRequestsData } from "../../hooks/useRequestData"
+import { useForm } from "../../hooks/useForm"
+import {Country} from "../../constants/country"
 
 
 export function CreateTripPage () {
 
+    const navigate = useNavigate()
+    const [form, onChange, clearForm] = useForm({
+        name: "",
+        planet: "",
+        date: "",
+        description: "",
+        durationInDays: ""
+     })
+
+
     return (
         <>
-            <p> Create Trip Page</p>
+            <p>Criar viagens</p>
+            <form>
+                <input 
+                    placeholder="nome"
+                    name="name"
+                />
+            </form>
         
         </>
     )
