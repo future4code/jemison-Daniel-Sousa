97f8c31750ddb7ebb6361bb5c1fb929b00b9a72e
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-08-16 12:11:05 -0300

    createTripPage validação de formulario

--- modulo4/projeto-labex/projeto-labex/src/pages/CreateTripPage/CreateTripPage.js
+++ modulo4/projeto-labex/projeto-labex/src/pages/CreateTripPage/CreateTripPage.js
@@ -8,6 +8,7 @@ import {Country} from "../../constants/country"
 export function CreateTripPage () {
 
     const navigate = useNavigate()
+    const [data] = useRequestsData("/trips")
     const [form, onChange, clearForm] = useForm({
         name: "",
         planet: "",
@@ -16,17 +17,48 @@ export function CreateTripPage () {
         durationInDays: ""
      })
 
+    const dateToday =  new Date().toISOString().split('T')[0]
+
+    const listOfPlanets = data && data.map((planet)=>{
+        return (
+            <option key={planet.id} value={planet.id}>
+                {planet.name}
+            </option>
+        )
+     })
 
     return (
         <>
             <p>Criar viagens</p>
             <form>
                 <input 
-                    placeholder="nome"
+                    placeholder={"Nome"}
                     name="name"
+                    required
+                />
+
+                <input 
+                    placeholder={"Descrição"}
+                    name="description"
+                    required
+                />
+
+                <input 
+                    placeholder={"Duração em dias"}
+                    type={"number"}
+                    name={"durationInDays"}
+                    required
                 />
             </form>
-        
+            <section>
+                <select
+                    placeholder={"Planeta"}
+                    name={"planet"}
+                >
+                    <option value={""}>Escolha um planeta</option>
+                    {listOfPlanets}
+                </select>
+            </section>
         </>
     )
 
