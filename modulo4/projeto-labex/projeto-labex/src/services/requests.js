af761827304b01dde5cf989173b033c5ce916912
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-08-15 22:49:31 -0300

    iniciando requições

--- modulo4/projeto-labex/projeto-labex/src/services/requests.js
+++ modulo4/projeto-labex/projeto-labex/src/services/requests.js
@@ -2,3 +2,23 @@ import axios from "axios"
 
 import {BASE_URL} from "../constants/urls"
 
+
+
+export const applyToTrip = (body, id) =>{
+    axios.post(`${BASE_URL}/trips/${id}/apply`, body)
+    .then((response) =>{
+        alert("Sucesso!")
+    }).catch((error) =>{
+        alert(error.response.message)
+    })
+}
+
+export const login = (body, navigate) =>{
+    axios.post(`${BASE_URL}/login`, body)
+    .then((response) =>{
+        localStorage.setItem("token", response.data.token)
+        //chamar função navigate
+    }).catch((error) =>{
+        alert(error.response.data.message)
+    })
+}
