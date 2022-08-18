73530580f90212cc908a799f5b137181b540e3fe
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-08-18 12:48:54 -0300

    adcionando useParame - useNaviagte - useProtegPage

--- modulo4/projeto-labex/projeto-labex/src/pages/TripDetailsPage/TripDetailsPage.js
+++ modulo4/projeto-labex/projeto-labex/src/pages/TripDetailsPage/TripDetailsPage.js
@@ -1,7 +1,14 @@
 import React from "react"
-
+import { useNavigate, useParams } from "react-router-dom"
+import { useProtectedPage } from "../../hooks/useProtectedPage"
+import {useRequestsData} from "../../hooks/useRequestData"
 
 export function TripDetailsPage () {
+    useProtectedPage()
+    const navigate = useNavigate()
+    const [id] =useParams()
+
+    const [getTripDetail, seGgetTripDetail] = useRequestsData(`/trips${id}`)
 
     return (
         <>
