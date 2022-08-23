b4be8be15de8ca5f05737fc06f9b6cabf952240f
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-08-23 09:24:57 -0300

    reiniciando logica das requisições

--- modulo4/projeto-labex/projeto-labex/src/services/requestPut.js
+++ modulo4/projeto-labex/projeto-labex/src/services/requestPut.js
@@ -2,23 +2,3 @@ import axios from "axios"
 
 import {BASE_URL} from "../constants/urls"
 
-export const decideCandidate = (idCandidate, idTrip, booleano, functionGetTrips) =>
-{
-    const body ={
-        approve: booleano
-    }
-
-    axios.put(`${BASE_URL}/trips/${idTrip}/candidates/${idCandidate}/decide`, body, {
-        headers:{
-            auth: localStorage.getItem('token')
-        }
-    }).then((response)=>{
-        alert("Sucesso! Decisão registrada")
-        functionGetTrips()
-    }).catch((error) =>{
-        alert("Error! Tente novamente")
-    })
-
-
-
-}
--- modulo4/projeto-labex/projeto-labex/src/services/requestsDelete.js
+++ modulo4/projeto-labex/projeto-labex/src/services/requestsDelete.js
@@ -2,16 +2,3 @@ import axios from "axios"
 
 import {BASE_URL} from "../constants/urls"
 
-export const deleteTrip = (idTrip, functionGetTrips) =>{
-    axios.delete(`${BASE_URL}/trips/${idTrip}`, {
-        headers:{
-            auth: localStorage.getItem("token")
-        }
-    }).then((response)=>{
-        alert("Sucesso! Viagem deletada")
-        functionGetTrips()
-    }).catch((error) => {
-        alert("Erro! Tente novamente")
-    })
-
-}
