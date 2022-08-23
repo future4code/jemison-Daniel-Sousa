7c6e383cee2f4b445d04e7972a3509a80cfe38c7
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-08-22 23:25:43 -0300

    iniciando estilização pageTripDetalis

--- modulo4/projeto-labex/projeto-labex/src/pages/TripDetailsPage/TripDetailsPage.js
+++ modulo4/projeto-labex/projeto-labex/src/pages/TripDetailsPage/TripDetailsPage.js
@@ -1,28 +1,31 @@
-import axios from "axios"
-import React from "react"
-import { useNavigate, useParams} from "react-router-dom"
-import { useProtectedPage } from "../../hooks/useProtectedPage"
-import {useRequestsData} from "../../hooks/useRequestData"
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import {MasterBox, ButtonBox, Container, ContainerItens, MainBox,Button} from "./Styled"
+import {goToLoginPage, goToListTripsPage} from "../../routes/coordinator"
+import logo from "../../assets/img/logo.png"
+import {Header} from "../../components/Header/Header"
+import {Footer} from "../../components/Footer/Footer"
 
 
 export function TripDetailsPage () {
-     useProtectedPage()
     
 
 
     return (
-        <>
-            <p> CONTROLE DE CONDITADOS  </p>
-            
-            <section>
-                <h3> Aprovar</h3>
-                
-            </section>
-
-            <section>
-                <h3> Aprovados</h3>
-                
-            </section>
-        </>
+        <MasterBox>
+               
+        <Container>
+            <ContainerItens >
+                <Header />
+                <MainBox>
+                  
+                    
+                    
+                </MainBox> 
+                <Footer/>
+            </ContainerItens>
+           
+        </Container>
+    </MasterBox>
     )
 }
