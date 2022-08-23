a27f263cf45a13c00e1cb49fc83c7051af0e343c
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-08-22 22:12:59 -0300

    adcionando estilo em page CreatTripage

--- modulo4/projeto-labex/projeto-labex/src/pages/CreateTripPage/CreateTripPage.js
+++ modulo4/projeto-labex/projeto-labex/src/pages/CreateTripPage/CreateTripPage.js
@@ -1,20 +1,17 @@
 import React from "react"
 import { useNavigate } from "react-router-dom"
+import { IoMdGlobe } from "react-icons/io";
 import { useForm } from "../../hooks/useForm"
 import {planets} from "../../constants/planet"
 import {goToAdminHomePag} from "../../routes/coordinator" 
+import {Header} from "../../components/Header/Header"
+import {Footer} from "../../components/Footer/Footer"
+import {MasterBox, ButtonBox, Container, ContainerItens, MainBox,Button, MainBoxIntns, Title, FormLogin,  SecaoPlanet  } from "./Styled"
 
 export function CreateTripPage () {
 
     const navigate = useNavigate()
     
-    const {form, onChange, clearForm} = useForm({
-        name: "",
-        planet: "",
-        date: "",
-        description: "",
-        durationInDays: ""
-     })
 
     const dateToday =  new Date().toISOString().split('T')[0]
 
@@ -27,67 +24,76 @@ export function CreateTripPage () {
      })
 
     return (
-        <>
-            <p>Criar viagens</p>
-            <form>
-                <input 
-                    placeholder={"Nome"}
-                    name="name"
-                    value={form.name}
-                    onChange ={onChange}
-                    pattern ={"^.{5,}$"}
-                    title = {"O nome da viagem deve ter no mínimo 5 caracteres"}
-                    required
-                />
+        <MasterBox>
+            <Container>
+                <ContainerItens>
+                    <Header />
+                    <MainBox>
+                        <Title>
+                            <IoMdGlobe color='black'/>
+                                Criar viagens
+                            <IoMdGlobe ccolor='black'/>
+                        </Title>
+                        <FormLogin>
+                            <form>
+                                <input 
+                                    placeholder={"Nome"}
+                                    name="name"
+                                    
+                                    pattern ={"^.{5,}$"}
+                                    title = {"O nome da viagem deve ter no mínimo 5 caracteres"}
+                                    required
+                                />
 
-                <input 
-                    placeholder={"Descrição"}
-                    name="description"
-                    value={form.description}
-                    onChange ={onChange}
-                    required
-                    pattern ={"^.{30,}$"}
-                    title = {"A descrição deve ter no mínimo 30 caracteres"}
-                    
-                />
+                                <input 
+                                    placeholder={"Descrição"}
+                                    name="description"
+                                    
+                                    required
+                                    pattern ={"^.{30,}$"}
+                                    title = {"A descrição deve ter no mínimo 30 caracteres"}
+                                    
+                                />
 
-                <input 
-                    placeholder={"Duração em dias"}
-                    type={"number"}
-                    name={"durationInDays"}
-                    value={form.durationInDays}
-                    onChange ={onChange}
-                    required
-                    min={50}
-                />
+                                <input 
+                                    placeholder={"Duração em dias"}
+                                    type={"number"}
+                                    name={"durationInDays"}
+                                    
+                                    required
+                                    min={50}
+                                />
 
-                 <section>
-                    <select
-                         placeholder={"Planeta"}
-                         name={"planet"}
-                         defaultValue ={""}
-                         onChange ={onChange}
-                     >
-                    <option value={""}>Escolha um planeta</option>
-                         {listOfPlanets}
-                    </select>
-                        <input
-                            placeholder={"Data"}
-                            type={"date"}
-                            name = {"date"}
-                            value ={form.date}
-                            onChange ={onChange}
-                            required
-                            min={dateToday}
-                        />
-                    </section>
-                    <section>
-                        <button onClick={() =>goToAdminHomePag(navigate)}> Voltar</button>
-                        <button type={"submit"}> Criar</button>
-                    </section>
-            </form>
-            
-        </>
+                                <SecaoPlanet>
+                                    <select
+                                        placeholder={"Planeta"}
+                                        name={"planet"}
+                                        defaultValue ={""}
+                                        
+                                    >
+                                    <option value={""}>Escolha um planeta</option>
+                                        {listOfPlanets}
+                                    </select>
+                                        <input
+                                            placeholder={"Data"}
+                                            type={"date"}
+                                            name = {"date"}
+                                        
+                                            required
+                                            min={dateToday}
+                                        />
+                                </SecaoPlanet>
+                                    <ButtonBox>
+                                        <Button onClick={() =>goToAdminHomePag(navigate)}> Voltar</Button>
+                                        <Button type={"submit"}> Criar</Button>
+                                    </ButtonBox>
+                            </form>
+                        </FormLogin>
+                    </MainBox> 
+                    <Footer/> 
+                </ContainerItens>    
+            </Container>    
+        </MasterBox>
     )
 
 
