743aa1d1b998153738773f368165d04d36d207c8
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-10-04 15:44:10 -0300

    Refeat: refatorando o prmeiro desafio

--- modulo05/revisao-javascript-backend/desafio_01/exercicio_01.js
+++ modulo05/revisao-javascript-backend/desafio_01/exercicio_01.js
@@ -3,6 +3,9 @@
 //2° - declarar um const com o valor em celsius ;
 
 //3° - termos condições (if, ou ternario) para buscarmos saber qual  para quem converter
+
+// 4° Inicia as tratativas de erros com a tipagem dos do parametros
+
 const aceitaString = (nome) => {
 
     if (typeof nome === "string") {
@@ -12,24 +15,6 @@ const aceitaString = (nome) => {
     }
 
 }
-
-// const aceitaNumeber = (num) => {
-
-//     if(typeof num !== "number"){
-//         return (`Erro. Parâmetro inválido, ${num}`)
-//     }
-
-// }
-
-// console.log(aceitaNumeber(5))
-
-
-
-
-// console.log(aceitaString(2))
-
-
-
 
 const conversorTempatura = (celsius , transfor) =>{
     
@@ -43,6 +28,7 @@ const conversorTempatura = (celsius , transfor) =>{
         return(`${celsius}° Celsius é equivalente a ${transforma}° Farenheit.`)
 
     } if (transfor === "k") {
+        
         const transforma = celsius + 273.15 
         return(`${celsius}° Celsius é equivalente a ${transforma}° Kelvis.`)
     } else {
@@ -51,4 +37,4 @@ const conversorTempatura = (celsius , transfor) =>{
 
 }
 
-  console.log(conversorTempatura("40", "K"))
+  console.log(conversorTempatura("40", "k"))
