935a86c6d1c6388404ba36e6b557b9c18d17adb9
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-10-04 13:00:42 -0300

    Fix: iniciando trativas de erro

--- modulo05/revisao-javascript-backend/desafio_01/exercicio_01.js
+++ modulo05/revisao-javascript-backend/desafio_01/exercicio_01.js
@@ -3,22 +3,52 @@
 //2° - declarar um const com o valor em celsius ;
 
 //3° - termos condições (if, ou ternario) para buscarmos saber qual  para quem converter
+const aceitaString = (nome) => {
 
-
-const conversorTempatura = (celsius=Number, transfor= Number) =>{
-    
-
-
-    if(transfor === "F"){
-        const transforma = (celsius * 9/5) + 32 
-        return(`${celsius}° Celsius é equivalente a ${transforma}° Farenheit.`)
-    } if (transfor === "k") {
-        const transforma = celsius + 273.15 
-        return(`${celsius}° Celsius é equivalente a ${transforma}° Kelvis.`)
-    } else {
-        return (`Dados incorretos`)
+    if (typeof nome === "string") {
+        return (`Paramentro errado, ${nome}`)
+    }else {
+        return nome
     }
 
 }
 
-console.log(conversorTempatura(40, "k"))
+// const aceitaNumeber = (num) => {
+
+//     if(typeof num !== "number"){
+//         return (`Erro. Parâmetro inválido, ${num}`)
+//     }
+
+// }
+
+// console.log(aceitaNumeber(5))
+
+
+
+
+// console.log(aceitaString(2))
+
+
+
+
+const conversorTempatura = (celsius , transfor) =>{
+    
+    
+
+    if(transfor === "F"){
+        
+        
+        const transforma = (celsius * 9/5) + 32 
+        
+        return(`${celsius}° Celsius é equivalente a ${transforma}° Farenheit.`)
+
+    } if (transfor === "k") {
+        const transforma = celsius + 273.15 
+        return(`${celsius}° Celsius é equivalente a ${transforma}° Kelvis.`)
+    } else {
+        return   aceitaString(transfor)
+    }
+
+}
+
+  console.log(conversorTempatura("40", "K"))
