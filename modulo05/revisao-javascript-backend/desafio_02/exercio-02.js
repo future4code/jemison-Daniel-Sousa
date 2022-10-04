effc4fbc34ecf75d121040b937b10372aace805a
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-10-04 16:14:20 -0300

    Fix: criando a estrutura da

---
+++ modulo05/revisao-javascript-backend/desafio_02/exercio-02.js
@@ -0,0 +1,15 @@
+const listaDeClientes = [
+	{ id: 1, nome: 'Fulano' },
+	{ id: 2, nome: 'Ciclano' },
+	{ id: 3, nome: 'Beltrano' },
+	{ id: 4, nome: 'Fulana' }
+]
+
+
+const novoCliente = (id, nome) => {
+    const novo = [...listaDeClientes, {id , nome}]
+
+    return novo
+}
+
+console.log(novoCliente(2, "Daniel"))
