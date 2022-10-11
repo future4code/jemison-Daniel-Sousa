c94cb32de2da75c7c63a84e3ed3dd62cb39e40f4
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-10-11 17:30:55 -0300

    Fix:

--- modulo5/node-package-json/Exercicios_aprofudamento/somaNumeros.js
+++ modulo5/node-package-json/Exercicios_aprofudamento/somaNumeros.js
@@ -1,12 +1,18 @@
+// Ainda nos exercícios 1 e 2 adicione verificações para garantir que os parâmetros passados estão corretos e informe ao usuário caso não estejam. Exemplo: "Esperava 2 parâmetros só recebi um."
+
+
 
 const n1=Number(process.argv[2])
 const n2=Number(process.argv[3])
 
 
 function somar(n1,n2){
-    return n1+n2;
+    if(n1 !== undefined || n2 !== undefined){
+        return n1 + n2
+    }else{
+        return `Falta um elemento`
+    }
+    
 }
 
-const resposta=somar(n1,n2)
-
-console.log(resposta);
+console.log(somar(n1,n2))
