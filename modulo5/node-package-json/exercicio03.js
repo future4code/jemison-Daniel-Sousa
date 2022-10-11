6c287308106bab5175ccf0b03f45237fc185be35
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-10-11 13:01:27 -0300

    Feat: se aplicado, resolução exercicio03

---
+++ modulo5/node-package-json/exercicio03.js
@@ -0,0 +1,14 @@
+//Crie uma aplicação Node que receba uma string representando uma tarefa. O programa deve adicionar a nova tarefa em uma variável que represente uma lista de tarefas. A lista de tarefas pode estar criada antes da execução do código. Após adicionar o item à lista, exiba a lista atualizada.
+
+const newTasks = process.argv[2];
+
+const todoList = [
+    'Estudar matematica'
+]
+
+function addNewTasks (tasksNew){
+    const newToDoList = [...todoList, tasksNew]
+    return newToDoList
+}
+
+console.log(addNewTasks(newTasks))
