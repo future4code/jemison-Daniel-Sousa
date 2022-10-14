7cb15faefe698c3885eac3e5d0295a37f3d2f7f6
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-10-14 20:03:40 -0300

    Fix: se aplicado, resolução do exercicio02

--- modulo5/intro-typescript/src/exercicio02.ts
+++ modulo5/intro-typescript/src/exercicio02.ts
@@ -1,6 +1,21 @@
-function soma2 (num1:number, num2:number){
+//Enunciado
+//A seguinte função em JavaScript pergunta ao usuário suas três cores favoritas e imprime no console um array que contenha essas três cores. Refatore a função para o Typescript.
 
-    return num1 + num2
+/* function imprimeTresCoresFavoritas() {
+    const cor1 = prompt("Insira sua primeira cor favorita")
+    const cor2 = prompt("Insira sua segunda cor favorita")
+    const cor3 = prompt("Insira sua terceira cor favorita")
+    console.log([cor1, cor2, cor3])
+ } */
+
+//1° - Criar um const = array[]
+
+const cores: string[] = [] // declarando um array de string fazio ;
+
+function imprimeTresCoresFavoritas (cor1:string, cor2:string, cor3:string) {
+    const imprimeCores = [...cores, cor1, cor2, cor3]
+     
+    return imprimeCores
 }
 
-console.log(soma2(2, 2))
+console.log(imprimeTresCoresFavoritas("Azul", "Verda", "Vermelha"))
