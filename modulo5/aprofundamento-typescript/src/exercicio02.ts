2525f197c8c7f77b981eed9cae1ea1fcbb110376
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-10-18 17:37:40 -0300

    Feat: reafazendo o projeto

---
+++ modulo5/aprofundamento-typescript/src/exercicio02.ts
@@ -0,0 +1,48 @@
+// function obterEstatisticas(numeros) {
+
+//     const numerosOrdenados = numeros.sort(
+//         (a, b) => a - b
+//     )
+
+//     let soma = 0
+
+//     for (let num of numeros) {
+//         soma += num
+//     }
+
+//     const estatisticas = {
+//         maior: numerosOrdenados[numeros.length - 1],
+//         menor: numerosOrdenados[0],
+//         media: soma / numeros.length
+//     }
+
+//     return estatisticas
+// }
+
+// a) Quais são as entradas e saídas dessa função? Copie a função para um arquivo .ts e faça a tipagem desses parâmetros
+
+//R= A entrada de dados, é um array
+
+const arry = [2, 3, 5, 79, 40]
+
+function obterEstatisticas(numeros:Array<number>) {
+
+    const numerosOrdenados = numeros.sort(
+        (a, b) => a - b
+    )
+
+    let soma:number = 0
+
+    for (let num of numeros) {
+        soma += num
+    }
+
+    const estatisticas:{maior:number,menor:number,media:number} = {
+        maior: numerosOrdenados[numeros.length - 1],
+        menor: numerosOrdenados[0],
+        media: soma / numeros.length
+    }
+
+    return estatisticas
+}
+console.log(obterEstatisticas(arry))
