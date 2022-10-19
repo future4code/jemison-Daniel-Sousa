073c8a9862700577a9ab07742afff389a8fcb14f
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-10-19 17:19:57 -0300

    Feat: se aplicado, resolucão exercicio07 b

--- modulo5/aprofundamento-typescript/src/exercicio07.ts
+++ modulo5/aprofundamento-typescript/src/exercicio07.ts
@@ -7,15 +7,29 @@ type prato = {
     ingredientes: Array<string>
 }
 
-const produtosCadastrados:Array<object> =[]
+const produtosCadastrados:Array<prato> =[]
 
 //a) Escreva uma função que permita cadastrar um produto. Salve os produtos em um array no escopo global.
 
 function cadastrasProdutos(nome:string,custo:number,valorVenda:number,ingredientes:Array<string>){
 
-   produtosCadastrados.push({nome, custo, valorVenda, ingredientes})
-    
-    return produtosCadastrados
+    produtosCadastrados.push({nome, custo, valorVenda, ingredientes})
+    return console.log(produtosCadastrados)
 }
-console.log(cadastrasProdutos("Feijoada", 10, 20, ["Feijão, tomate,calabresa, carne de sol"]))
+cadastrasProdutos("Feijoada", 10, 20, ["Feijão", "tomate","calabresa", "carne de sol"])
+// cadastrasProdutos("Arooz", 30, 60, ["Arroz"])
 
+
+
+//b) Escreva uma função que recebe um nome e devolve o valor do produto com este nome.
+
+function buscaNome (nomeProduto:string){
+    const filterNome = produtosCadastrados.filter((item)=>{
+       return item.nome === nomeProduto? item.nome && item.valorVenda : `Não há nada`
+    })
+
+    return filterNome
+}
+
+console.log('--ISSO É A BUSCA--')
+console.log(buscaNome("Arroz"))
