cb37d47abfc34f60e40745271520800204523775
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-10-25 11:20:21 -0300

    Feat: se aplicado, resolucao exercico01

--- modulo5/intro-express/src/index.ts
+++ modulo5/intro-express/src/index.ts
@@ -5,6 +5,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//2 - Acesse a API do JSONPlaceholder e observe os endpoints que buscam usuários. No seu projeto, crie uma variável de tipo para representar esse recurso. Eles devem possuir as seguintes propriedades:
+
+type usuarios = {
+    id: number,
+    name: string,
+    phone:string,
+    email: string,
+    website:string
+}
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(3003, () => {
-    console.log("Hello from Express")
+    console.log("Server is running in http://localhost:3003");
 });
