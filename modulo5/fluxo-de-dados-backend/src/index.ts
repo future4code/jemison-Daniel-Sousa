c92a28b2c32970502c8d4bb055ac46679305664d
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-11-01 15:23:37 -0300

    Feat: exercicio01

--- modulo5/fluxo-de-dados-backend/src/index.ts
+++ modulo5/fluxo-de-dados-backend/src/index.ts
@@ -1,10 +1,19 @@
-import express, { request, response } from "express";
+
 import cors from "cors";
+import express, { request, response } from "express";
+import {Product,ProductList} from './data'
+import { AddressInfo } from "net";
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+//1 - Crie uma nova API do zero e desenvolva um endpoint de teste com método GET no path “/test” que retorna a mensagem: “Servidor rodando na porta (colocar porta)!”. 
+
+app.get("/test", (request,response) =>{
+    response.send("Servidor rodando na porta")
+})
 
 
 
@@ -12,8 +21,11 @@ app.use(express.json());
 
 
 
-
-
-app.listen(3003, () => {
-    console.log("Servidor rodando na porta http://localhost:3003");
-});
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+      const address = server.address() as AddressInfo;
+      console.log(`Servidor rodando na porta http://localhost:${address.port}`);
+    } else {
+      console.error(`Falha ao iniciar o servidor.`);
+    }
+  });;
